806c9bf1262864e3143ae8c930f3bf3c88cccf07
Justin Borek <justinborek@gmail.com>
2017-10-26 13:38:57 -0500

    corrected file usage, split off a login from the main app folder, added user-data and css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login.js'
 
 class App extends Component {
   render() {
@@ -9,13 +10,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h1><input type = 'text'></input></h1>
-        <h2><input type = 'text'></input></h2>
-        <h3><button>Login</button></h3>
-        <h4><button>Get Started</button></h4>
-        <h5><button>I Just Need A Quick Calculation!</button></h5>
-
+        </header> 
+        <Login />
       </div>
     );
   }
---
+++ src/user-data.js
@@ -0,0 +1,9 @@
+const Users = [
+  {
+    username: 'justinborek',
+    password: 'password',
+    firstName: 'Justin',
+    lastName: 'Borek',
+    email: 'justinborek@gmail.com'
+  }
+]
