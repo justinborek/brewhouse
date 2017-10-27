5c03159c127d7e04e4dc1392839644149b0a4811
Justin Borek <justinborek@gmail.com>
2017-10-27 18:16:34 -0500

    new commit

--- src/splash/splash.js
+++ src/splash/splash.js
@@ -3,6 +3,8 @@ import './splash.css';
 import userData from '../user-data.js';
 import NavBar from '../navbar/navbar.js';
 import image1 from '../images/homebrew1.png';
+import { Button } from 'react-bootstrap';
+
 
 class Splash extends Component {
 
@@ -10,8 +12,22 @@ render() {
   return (
     <div className="splashPage">
       <NavBar/>
-      <div className = "section section1">
-      <p>A better way to brew.</p>
+      <div className = "section1">
+        <div className = 'section1_Content'>
+          <h1> A better way to brew. </h1>
+          <button><span>Try it!</span></button>
+        </div>
+      </div>
+      <div className = "section2">
+      </div>
+      <div className = "section3">
+      </div>
+      <div className = "section4">
+      </div>
+      <div className = "footer">
+        <button><i class="fa fa-facebook-official" aria-hidden="true"></i></button>
+        <button><i class="fa fa-twitter" aria-hidden="true"></i></button>
+        <button><i class="fa fa-instagram" aria-hidden="true"></i></button>
       </div>
     </div>
   );
