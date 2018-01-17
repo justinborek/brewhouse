80a80cd8ed106a1273f6287a1ed58f158fbab405
Justin Borek <justinborek@gmail.com>
2018-01-17 16:15:45 -0600

    recipe selection modal working and redirects to main page, need to implement the put on the backend

--- src/components/recipeSelect.js
+++ src/components/recipeSelect.js
@@ -1,34 +1,91 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { Modal, Button, Popover, Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 import '../styles/recipeSelect.css';
 
 let recipes = [];
 
-class RecipeSelect extends Component {
-  componentDidMount() {
-    axios
-    .get('http://localhost:3030/recipes')
-    .then(response => {
-      response.data.forEach(recipe => {
-        recipes.push(recipe);
-      });
-    })
-    .catch(err => {
-      console.log(`Error in recipeSelect componentDidMount ${err}`);
+let counter = 0;
+
+axios
+  .get('http://localhost:3030/recipes')
+  .then(response => {
+    response.data.forEach(recipe => {
+      recipes[counter] = recipe;
+      counter++;
     });
-  };
+  })
+  .catch(err => {
+    console.log(`Error in recipeSelect componentDidMount ${err}`);
+  });
+
+class RecipeSelect extends Component {
+  constructor(...args) {
+		super(...args);
+
+		this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.chooseRecipe = this.chooseRecipe.bind(this);
+    this.moveToBrewhouse = this.moveToBrewhouse.bind(this);
+    
+
+    this.state = { showModal: false };
+    this.state = { recipeChoice: {} };
+	}
+
+	handleClose() {
+		this.setState({ showModal: false });
+	}
+
+	handleShow() {
+		this.setState({ showModal: true });
+  }
+
+  chooseRecipe(recipe) {
+    this.setState({ recipeChoice: recipe });
+  }
+
+  moveToBrewhouse() {
+    let fermArr = this.props.user.equipment.fermenters;
+    for (let i = 0; i < fermArr.length; i++) {
+      if (!fermArr[i].activeRecipe) {
+        fermArr[i].activeRecipe = this.state.recipeChoice;
+        return;
+      }
+      this.handleClose();
+      this.props.history.goBack();
+    };
+  }
   
   render() {
-    console.log(recipes);
-    console.log(recipes.length);
     return (
       <div className = "recipeSelect">
-        <h1>What would you like to brew?</h1>
-        <div>
+        <Modal show={this.state.showModal} onHide={this.handleClose}>
+					<Modal.Body>
+						<h4>Let's make sure you're ready to brew.</h4>
+						<p>
+							Double check that you have all your ingredients.
+						</p>
+						<p>
+							Once you click ready, your brew day will begin. 
+						</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={this.handleClose}>Not Yet!</Button>
+            <Button onClick={this.moveToBrewhouse}>Ready!</Button>
+					</Modal.Footer>
+				</Modal>
+      <h1>What would you like to brew?</h1>
+        <div className = "recipeButton">
           { recipes.map((item) => {
-            console.log(item);
+            return (
+              <button onClick={() => {
+                this.chooseRecipe(item);
+                this.handleShow();
+              }}>{item.title}</button>
+            )
           })}
         </div>
       </div>
